253d6ae2f31eeacfe8064fa7de122babfc2081a5
Rahul Makhija <rahuls360@gmail.com>
2019-04-01 22:10:03 +0530

    add finished code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class App extends Component {
+  increment = () => {
+    this.props.dispatch({ type: "INCREMENT" });
+  };
+  decrement = () => {
+    this.props.dispatch({ type: "DECREMENT" });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="box">
+        <button onClick={this.increment}>+</button>
+        {this.props.count}
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  count: state.count
+});
+
+export default connect(mapStateToProps)(App);
